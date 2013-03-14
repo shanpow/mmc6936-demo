1e069b05c4b9442a2975b20c592c129b54e75691
Chris Lane <chris@chris-allen-lane.com>
2013-03-14 19:06:47 -0400

    Added the flexslider, page-turn, and picstrips demos.

---
+++ js/jquery-plugins/page-turn/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#magazine').turn({gradients: true, acceleration: true});
+});
---
+++ js/jquery-plugins/picstrips/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#s").picstrips({
+        splits  : 10,
+        hgutter : '10px',
+        vgutter : '10px',
+        bgcolor : '#f00'
+    });
+});
